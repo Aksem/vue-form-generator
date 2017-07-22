7c29a4589e01075d3be4091d34cf71a20afc870b
Vladyslav Hnatiuk <aders1234@gmail.com>
2017-07-22 17:50:00 +0200

    Added DatePicker optional field(based on vuejs-datepicker)

---
+++ src/fields/optional/fieldDatePicker.vue
@@ -0,0 +1,37 @@
+<template>
+<datepicker :placeholder="schema.placeholder" name="schema.inputName" readonly="schema.readonly" id="getFieldID(schema)" :format="getDateFormat()" v-model="value"></datepicker>
+</template>
+
+<script>
+    /* global $ */
+    import abstractField from "../abstractField";
+    import Datepicker from 'vuejs-datepicker';
+
+
+    let format = "yyyy-MM-dd";
+
+    export default {
+        mixins: [ abstractField ],
+        components: {
+            datepicker: Datepicker
+        },
+
+        methods: {
+            getDateFormat: function () {
+                if (this.schema.datePickerOptions && this.schema.datePickerOptions.format) {
+                    return this.schema.datePickerOptions.format;
+                } else {
+                    return format;
+                }
+            }
+        },
+        beforeDestroy() {
+            if (window.$ && window.$.fn.datetimepicker){
+                $(this.$el).data("DatePicker").destroy();
+            }
+        }
+    };
+</script>
+
+<style>
+</style>
